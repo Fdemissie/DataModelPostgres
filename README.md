075975442014b8d132e67aaf20b0248109916ef0
Fishwond <48863695+Fishwond@users.noreply.github.com>
2019-04-13 18:02:03 +0200

    updated readme and SQL standard

--- README.md
+++ README.md
@@ -1,29 +1,52 @@
-# Data Modeling with Postgres for Sparkify
+# Postgres Data model for Sparkify
 ****
 
 # Purpose
 ****
 
-For a startup company sparkify there is a need to analyze the data they've been collecting on songs and user activity on their new music streaming app. The analytics team is particularly interested in understanding what songs users are listening to. Currently, they don't have an easy way to query their data, which resides in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in their app.
+The purpose of the newly created database is to move user activity and songs metadata which is stored in JSON format in sparkify music streaming app to well structured tables. Having properly desiged database will help to easily query specifc data and give an insite to Sparkify what songs users are listening to. 
 
 
 # Approach
 ****
-In order to optimize queries on song play analysis, star schema is used with  fact table(songplays) and dimension tables(songs, users, artists and time). An ETL pipeline is create that transfers data from JSON files in two local directories into these tables in Postgres using Python and SQL. 
+In order to optimize queries on song play analysis, star schema is used with  fact table(songplays) and dimension tables(songs, users, artists and time). Star schema has simpler structure which makes it easier to understand and good query effectiveness because we need small numer of tables to join. An ETL pipeline is used to transfer data from JSON files in two local directories into these tables in Postgres using Python and SQL. 
 
-![Star Schema](starSchema.PNG)
+![Star Schema](resource/starSchema.PNG)
+
+
+#### sql_queries.py  
+Drops all tables if any exists then create all five tables with the rigt data type and constraints, once the tables are created, new recordes will be inserted into the tables - expects input values. 
+
+#### create_tables.py
+Imports queries from sql_queries.py and create a database connection, drop all tables and create all tables.
+
+#### etl.py
+Is the ETL pipeline that access log and song JSON files from local directory and iterate through them, extract the records, transofrm them to the right format and send the records to sql_queries.py to be inserted into the corsponding tables.   
+
+### steps to run pipeline
+- %run create_tables.py
+- %run etl.py
 
 # Sample query
 ***
+`SELECT * FROM songplays LIMIT 5;`
+
+![songplays](resource/songplays.PNG)
+
 `SELECT count(*) FROM songplays WHERE level = 'paid' AND location = 'Tampa-St. Petersburg-Clearwater, FL' `
 
 Result:
 count
 289
 
-`SELECT user_id, level, song_id, artist_id FROM songplays WHERE song_id <> 'None' AND level = 'paid' LIMIT 5`
+`SELECT * FROM users LIMIT 5;`
+![users](resource/users.PNG)
 
-Result:
+`SELECT * FROM artists LIMIT 5;`
+![artists](resource/artists.PNG)
 
-##### user_id 	level 	song_id 	artist_id
-##### 15 	paid 	SOZCTXZ12AB0182364 	AR5KOSW1187FB35FF4
+`SELECT * FROM songs LIMIT 5;`
+![songs](resource/songs.PNG)
+
+`SELECT * FROM users LIMIT 5;`
+![songs](resource/users.PNG)
--- sql_queries.py
+++ sql_queries.py
@@ -8,43 +8,91 @@ time_table_drop = "DROP TABLE  IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY, start_time varchar,\
-                            user_id varchar , level varchar, song_id varchar , artist_id varchar, session_id varchar, location varchar, \
-                            user_agent varchar)""")
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (
+                                songplay_id SERIAL PRIMARY KEY, 
+                                start_time varchar,
+                                user_id varchar, 
+                                level varchar, 
+                                song_id varchar , 
+                                artist_id varchar ,
+                                session_id varchar, 
+                                location varchar, 
+                                user_agent varchar)
+                                """)
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id varchar, first_name varchar, last_name varchar, gender varchar, \
-                        level varchar)""")
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (
+                            user_id varchar PRIMARY KEY, 
+                            first_name varchar NOT NULL, 
+                            last_name varchar NOT NULL, 
+                            gender varchar NOT NULL, 
+                            level varchar NOT NULL
+                            )""")
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, artist_id varchar, year int, duration float) """)
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (
+                            song_id varchar PRIMARY KEY, 
+                            title varchar NOT NULL, 
+                            artist_id varchar NOT NULL,
+                            year int NOT NULL, 
+                            duration float NOT NULL
+                            )""")
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar, artist_name varchar, artist_location varchar, \
-                        artist_latitude real, artist_longitude real)""")
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (
+                              artist_id varchar PRIMARY KEY, 
+                              artist_name varchar NOT NULL,
+                              artist_location varchar NOT NULL,
+                              artist_latitude real NOT NULL,
+                              artist_longitude real NOT NULL
+                          )""")
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time varchar, hour varchar, day varchar, week varchar, month varchar,\
-                        year varchar, weekday varchar)""")
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (
+                            start_time varchar PRIMARY KEY, 
+                            hour varchar NOT NULL, 
+                            day varchar NOT NULL,
+                            week varchar NOT NULL,
+                            month varchar NOT NULL,
+                            year varchar NOT NULL,
+                            weekday varchar NOT NULL
+                        )""")
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) \
-                            VALUES(DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s )""")
+songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+                            VALUES(DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s) 
+                            """)
 
-user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) \
-                        VALUES(%s, %s, %s, %s, %s)""")
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
+                        VALUES(%s, %s, %s, %s, %s) 
+                        ON CONFLICT (user_id)
+                        DO UPDATE SET (first_name, last_name, gender, level) = (EXCLUDED.first_name, EXCLUDED.last_name, EXCLUDED.gender, EXCLUDED.level)
+                        """)
 
-song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) \
-                        VALUES(%s, %s, %s, %s, %s)""")
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)
+                        VALUES(%s, %s, %s, %s, %s) 
+                        ON CONFLICT (song_id)
+                        DO UPDATE SET (title, artist_id, year, duration) = (EXCLUDED.title, EXCLUDED.artist_id, EXCLUDED.year, EXCLUDED.duration)
+                    """)
 
-artist_table_insert = ("""INSERT INTO artists (artist_id, artist_name, artist_location, artist_latitude, artist_longitude) \
-                        VALUES(%s, %s, %s, %s, %s)""")
+artist_table_insert = ("""INSERT INTO artists (artist_id, artist_name, artist_location, artist_latitude, artist_longitude)
+                        VALUES(%s, %s, %s, %s, %s) 
+                        ON CONFLICT (artist_id)
+                        DO UPDATE SET (artist_name, artist_location, artist_latitude, artist_longitude) = (EXCLUDED.artist_name, EXCLUDED.artist_location,
+                            EXCLUDED.artist_latitude, EXCLUDED.artist_longitude)
+                        """)
 
 
-time_table_insert = ("""INSERT INTO time (start_time, hour, day , week , month, year, weekday)VALUES(%s, %s, %s, %s, %s, %s, %s)""")
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday)\
+                        VALUES(%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time) DO NOTHING""")
 
 # FIND SONGS
 
-song_select = ("""SELECT songs.song_id, artists.artist_id FROM songs INNER JOIN artists ON \
-                songs.artist_id = artists.artist_id WHERE title = %s AND \
-                artist_name = %s AND duration = %s""")
+song_select = ("""SELECT songs.song_id, artists.artist_id 
+                  FROM songs 
+                      INNER JOIN artists 
+                      ON songs.artist_id = artists.artist_id 
+                      WHERE title = %s 
+                      AND   artist_name = %s 
+                      AND duration = %s
+                """)
 
 
 
